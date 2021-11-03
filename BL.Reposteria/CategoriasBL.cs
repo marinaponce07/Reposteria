0d7c819b562232c0194c9bd3c0e8278cb901ef44
marinaponce07 <90664135+marinaponce07@users.noreply.github.com>
2021-11-03 01:02:59 -0600

    Add files via upload

---
+++ BL.Reposteria/CategoriasBL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Reposteria
+{
+    public class CategoriasBL
+    {
+        Contexto _contexto;
+        public BindingList<CategoriaBL> ListaCategorias { get; set; }
+
+        public CategoriasBL()
+        {
+            _contexto = new Contexto();
+            ListaCategorias = new BindingList<CategoriaBL>();
+        }
+
+        public BindingList<CategoriaBL> ObtenerCategorias()
+        {
+            _contexto.Categorias.Load();
+            ListaCategorias = _contexto.Categorias.Local.ToBindingList();
+
+            return ListaCategorias;
+        }
+    }
+
+    public class CategoriaBL
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ BL.Reposteria/DatosdeInicioBL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BL.Reposteria.SeguridadBL;
+
+namespace BL.Reposteria
+{
+    public class DatosdeInicioBL: CreateDatabaseIfNotExists<Contexto>
+    {
+        protected override void Seed(Contexto contexto)
+        {
+
+            var usuarioAdmin = new Usuario();
+            usuarioAdmin.Nombre = "admin";
+            usuarioAdmin.Contrasena = "123";
+            contexto.Usuarios.Add(usuarioAdmin);
+
+            var categoria1 = new CategoriaBL();
+            categoria1.Descripcion = "Chocolate";
+            contexto.Categorias.Add(categoria1);
+
+            var categoria2 = new CategoriaBL();
+            categoria2.Descripcion = "Frutas";
+            contexto.Categorias.Add(categoria2);
+
+            var categoria3 = new CategoriaBL();
+            categoria3.Descripcion = "Queso";
+            contexto.Categorias.Add(categoria3);
+
+            var tipo1 = new Tipo();
+            tipo1.Descripcion = "Pasteles";
+            contexto.Tipos.Add(tipo1);
+
+            var tipo2 = new Tipo();
+            tipo2.Descripcion = "Budines";
+            contexto.Tipos.Add(tipo2);
+            
+
+            base.Seed(contexto);
+        }
+    }
+}
